bbd0d08686150ce12de6f34f7c7f75ca42161d4c
memanja <48172171+memanja@users.noreply.github.com>
2024-06-26 23:00:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ConsoleFun',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
-        'pyfiglet',
         'tabulate',
         'prettytable',
+        'colorama',
     ],
     entry_points={
         'console_scripts': [
